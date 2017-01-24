c98279484c0d4381302b390e2fbe047d8e6ff4d0
ameyaraje <ameyacraje@gmail.com>
2017-01-24 00:58:04 -0800

    more on 24th jan

---
+++ 459.java
@@ -0,0 +1,23 @@
+/*  
+    One effing test case fails with the original method tried.
+    Idices count up from 1 to len/2
+*/
+public class Solution {
+    public boolean repeatedSubstringPattern(String str) {
+        int len = str.length();
+    	for(int i=len/2 ; i>=1 ; i--) {
+    		if (len%i == 0) {
+    			int m = len/i;
+    			String subS = str.substring(0,i);
+    			int j;
+    			for(j=1;j<m;j++) {
+    				if(!subS.equals(str.substring(j*i,i+j*i))) 
+    				    break;
+    			}
+    			if(j==m)
+    			    return true;
+    		}
+    	}
+    	return false;
+    }
+}
